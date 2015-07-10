605a61228953e751e1c2b8513b9d829ee1ce1e70
Ben Gamari <ben@smart-cactus.org>
2015-07-10 23:20:48 +0200

    Initial commit

---
+++ re-boot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ghc79_libs="base ghc-prim integer-gmp integer-simple template-haskell"
+
+function to_ghc79() {
+        for d in $ghc79_libs; do
+                rm -Rf libraries/$d/.git
+        done
+        ./sync-all checkout master
+}
+
+function from_ghc79() {
+        for d in $ghc79_libs; do
+                rm -Rf libraries/$d
+        done
+        ./sync-all get
+        ./sync-all checkout ghc-7.8
+        ./boot
+}
+
