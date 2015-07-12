f6180edf1623bc13a0759c1dad20bee69dee03b3
Ben Gamari <bgamari.foss@gmail.com>
2015-07-12 06:46:54 -0400

    centos: parametrize on architecture

--- mkchroot-centos.sh
+++ mkchroot-centos.sh
@@ -1,7 +1,23 @@
 #!/bin/bash -e
 
 # See https://zaufi.github.io/administration/2014/06/10/howto-make-a-centos-chroot/
-#sudo apt-get install yum centos
+
+# Needs,
+#    sudo apt-get install yum rpm
+
+arch=i386
+case $arch in
+    amd64|x86_64)
+        centos_release="http://mirror.centos.org/centos/6/os/x86_64/Packages/centos-release-6-6.el6.centos.12.2.x86_64.rpm "
+        ;;
+    i386)
+        centos_release="http://mirror.centos.org/centos/6/os/x86_64/Packages/centos-release-6-6.el6.centos.12.2.x86_64.rpm "
+        ;;
+    *)
+        echo "Unknown architecture $arch"
+        exit 1
+        ;;
+esac
 
 if [ -z "$1" ]; then
     echo "Usage: $0 [dest]"
@@ -16,7 +32,7 @@ rpm --root=$dest --rebuilddb
 sudo chown $user $dest
 sudo chmod u+rwx $dest
 
-curl http://mirror.centos.org/centos/6/os/x86_64/Packages/centos-release-6-6.el6.centos.12.2.x86_64.rpm > $dest/tmp/centos-release.rpm
+curl $centos_release > $dest/tmp/centos-release.rpm
 sudo rpm --root=$dest --nodeps --install $dest/tmp/centos-release.rpm
 sudo sed -i -e 's/gpgcheck=1/gpgcheck=0/' $dest/etc/yum.repos.d/CentOS-Base.repo
 
