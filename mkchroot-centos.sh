0e077cdd640f079012654a25a44566a9caff7495
Ben Gamari <bgamari.foss@gmail.com>
2015-07-16 17:44:09 -0400

    32-bit centos seems to work

--- mkchroot-centos.sh
+++ mkchroot-centos.sh
@@ -9,14 +9,20 @@ function usage() {
     echo "Usage: ARCH=[i386|amd64] $0 [dest]"
 }
 
+# Yum unfortunately provides no way to override $basearch other than setting uname.
+# We use setarch to accomplish this.
 case $ARCH in
     amd64|x86_64)
         centos_release="http://mirror.centos.org/centos/6/os/x86_64/Packages/centos-release-6-6.el6.centos.12.2.x86_64.rpm "
         arch=amd64
+        personality=x86-64
+        setarch="setarch x86_64"
         ;;
     i386)
         centos_release="http://mirror.centos.org/centos/6/os/i386/Packages/centos-release-6-6.el6.centos.12.2.i686.rpm"
         arch=i386
+        personality=x86
+        setarch="setarch i686"
         ;;
     *)
         echo "Unknown architecture $arch"
@@ -36,18 +42,19 @@ user=$(id -un)
 mkdir $dest
 rpm --root=$dest --rebuilddb
 
+echo "Using centos-release $centos_release"
 curl $centos_release > $dest/centos-release.rpm
 sudo rpm --root=$dest --nodeps --install $dest/centos-release.rpm
 
-sudo yum --installroot=$dest --nogpg update
-sudo yum --installroot=$dest --nogpg -y install yum
+sudo $setarch yum --installroot=$dest --nogpg update
+sudo $setarch yum --installroot=$dest --nogpg -y install yum
 
 sudo chown $user $dest
 sudo chmod u+rwx $dest
 cat >$dest/activate <<EOF
 #!/bin/bash
 MY_CHROOT=$dest
-sudo systemd-nspawn -D\$MY_CHROOT \$@
+sudo systemd-nspawn --personality=$personality -D\$MY_CHROOT \$@
 EOF
 chmod ugo+rx $dest/activate
 
@@ -55,6 +62,7 @@ chmod ugo+rx $dest/activate
 # Rebuild the package database with the CentOS RPM
 $dest/activate bash -e <<EOF
 rm -rf /var/lib/rpm
+echo "exclude=udev" >> /etc/yum.conf
 rpm --rebuilddb
 rpm --nodeps -i /centos-release.rpm
 rm /centos-release.rpm
