042df4bbe691f4f9005b163311478b9b8568b8ca
Ben Gamari <bgamari.foss@gmail.com>
2015-07-22 17:24:49 -0400

    bin-release: Refactor

--- rel-eng/bin-release.sh
+++ rel-eng/bin-release.sh
@@ -6,12 +6,11 @@
 #   export ver="7.10.2"
 #   wget http://downloads.haskell.org/~ghc/$ver/ghc-$ver-src.tar.bz2
 #   wget http://downloads.haskell.org/~ghc/$ver/ghc-$ver-testsuite.tar.bz2
-#   bash bin-release.sh
+#   NTHREADS=4 bash bin-release.sh
 
 mkdir -p bin-dist-$ver
 
 cd bin-dist-$ver
-#rm -R ghc-$ver
 
 function setup_debian() {
     sudo apt-get install dblatex docbook-xsl
@@ -21,26 +20,32 @@ function setup_redhat() {
     cat centos-6.6.pkgs | sudo xargs yum install -y
 }
 
-if [ ! -z "$skip_pkgs" ]; then
-    if ! grep CentOS /etc/issue; then
-        setup_redhat
-    elif ! grep Debian /etc/issue; then
-        setup_debian
-    elif ! grep Ubuntu /etc/issue; then
-        setup_debian
-    else
-        echo "Unknown distribution"
+function prepare() {
+    if [ ! -z "$skip_pkgs" ]; then
+        if ! grep CentOS /etc/issue; then
+            setup_redhat
+        elif ! grep Debian /etc/issue; then
+            setup_debian
+        elif ! grep Ubuntu /etc/issue; then
+            setup_debian
+        else
+            echo "Unknown distribution"
+        fi
     fi
-fi
 
+    cabal install hscolour
+    rm -Rf ghc-$ver
+    tar -jxf ../ghc-$ver-src.tar.bz2
+    tar -jxf ../ghc-$ver-testsuite.tar.bz2
+}
 
-cabal install hscolour
+function do_build() {
+    if [ -z "$NTHREADS" ]; then
+        NTHREADS=1
+    fi
 
-tar -jxf ../ghc-$ver-src.tar.bz2
-tar -jxf ../ghc-$ver-testsuite.tar.bz2
-cd ghc-$ver
-
-cat > mk/build.mk <<EOF
+    cd ghc-$ver
+    cat > mk/build.mk <<EOF
 V=1
 HADDOCK_DOCS=YES
 LATEX_DOCS=YES
@@ -51,21 +56,27 @@ BUILD_DOCBOOK_PS=YES
 BeConservative=YES
 EOF
 
-if [ -z "$NTHREADS" ]; then
-    NTHREADS=1
+    ./configure      2>&1 | tee ../conf.log
+    make -j$NTHREADS 2>&1 | tee ../make.log
+    make binary-dist 2>&1 | tee ../binary-dist.log
+    cd ..
+}
+
+function rebuild() {
+    rm -Rf test
+    mkdir test
+    tar -jx -C test -f ../ghc-$ver/ghc-$ver-*.tar.bz2
+    cd test/ghc-$ver
+    ./configure --prefix=$(realpath ..)/inst
+    make
+    make install
+    echo "Things look good."
+}
+
+if [ $# == 0 ]; then
+    prepare
+    do_build
+    rebuild
+else
+    $1
 fi
-
-./configure      2>&1 | tee ../conf.log
-make -j$NTHREADS 2>&1 | tee ../make.log
-make binary-dist 2>&1 | tee ../binary-dist.log
-
-cd ..
-rm -R test
-mkdir test
-tar -jx -C test -f ../ghc-$ver/ghc-$ver-$thing.tar.bz2
-cd ghc-$ver
-./configure --prefix=$(realpath ..)/inst
-make
-make install
-
-echo "Things look good."
