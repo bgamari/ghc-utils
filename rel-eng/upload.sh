3c4334e2829b0b4ca3f3d277f3f4dbccb0d4c586
Ben Gamari <ben@smart-cactus.org>
2015-07-28 17:51:33 +0200

    upload: Kill docs/index.html

--- rel-eng/upload.sh
+++ rel-eng/upload.sh
@@ -40,7 +40,9 @@ function prepare_docs() {
     ln -s users_guide docs/html
     cp -R ghc-$ver/utils/haddock/doc/haddock docs/haddock
     rm -R ghc-$ver
+
     tar -jxf docs/libraries.html.tar.bz2 -C docs
+    rm docs/index.html
 }
 
 gen_hashes
