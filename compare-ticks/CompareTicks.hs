1ca4fd3270b51f3b7c36b512e64b55810e2a414a
Ben Gamari <ben@smart-cactus.org>
2016-07-04 23:06:53 +0200

    compare-ticks: Clean things up

--- compare-ticks/CompareTicks.hs
+++ compare-ticks/CompareTicks.hs
@@ -4,144 +4,13 @@
 module Main where
 
 import Numeric
-import Text.Trifecta
 import Data.List (sortBy, transpose, intercalate)
-import Data.Char (isSpace)
 import Data.Monoid
-import Control.Applicative
 import qualified Options.Applicative as O
-import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Data.Map as M
 import qualified Data.Set as S
-
-data TickyReport = TickyReport { frames :: [TickyFrame] }
-                   deriving (Show)
-
-data TickyFrame = TickyFrame { entries   :: Integer
-                             , alloc     :: Integer
-                             , allocd    :: Integer
-                             , arguments :: String
-                             , stgName   :: StgName
-                             }
-                  deriving (Show)
-
-
--- | The name of a module
-data ModuleName = ModName { modulePackage :: String
-                          , moduleName    :: String
-                          }
-                deriving (Show)
-
-pprModuleName :: ModuleName -> String
-pprModuleName (ModName pkg name) = pkg<>":"<>name
-
--- | An STG name
---
--- An exported name like @ghc-7.11:CmdLineParser.runCmdLine{v rWL}@ is,
---
--- @
--- StgName { definingModule = Just $ ModName (Just "ghc-7.11") "CmdLineParser"
---         , name = "runCmdLine"
---         , signature = "v rWL"
---         , parent = Nothing
---         }
--- @
---
--- A non-exported name like @sat_s5EP{v} (ghc-7.11:CmdLineParser) in rXu@ is,
---
--- @
--- StgName { definingModule = Nothing
---         , name = "sat_s5EP"
---         , signature = "v"
---         , parent = "rXu"
---         }
--- @
-data StgName = StgName { stgnExported       :: Bool
-                       , _stgnDefiningModule :: ModuleName
-                       , _stgnName          :: String
-                       , stgnSignature      :: String
-                       , stgnParent         :: Maybe String
-                       }
-             | Top
-             deriving (Show)
-
-stgnName :: StgName -> String
-stgnName (StgName {_stgnName=n}) = n
-stgnName Top = "Top"
-
-pprStgName :: StgName -> String
-pprStgName Top = "Top"
-pprStgName s@(StgName exported mod name sig parent) =
-    name<>" ("<>pprModuleName mod<>")"
-
-stgnDefiningModule :: StgName -> ModuleName
-stgnDefiningModule Top = ModName "" ""
-stgnDefiningModule s@(StgName _ mod _ _ _) = mod
-
-parseReport :: T.Text -> TickyReport
-parseReport s =
-    let ls = T.lines s
-        _:tableLines = dropWhile (\l -> not $ "---------------------" `T.isPrefixOf` l) ls
-        parseIt s = case parseString parseFrame mempty (T.unpack s) of
-          Success a -> a
-          Failure doc -> error $ show doc
-    in TickyReport $ map parseIt
-                   $ filter (not . T.null)
-                   $ takeWhile (\l -> not $ "*****************" `T.isPrefixOf` l)
-                   $ tableLines
-
-spacesThen :: Parser a -> Parser a
-spacesThen parser = skipMany space *> parser
-
-named :: String -> Parser a -> Parser a
-named n p = p <?> n
-
-parseFrame :: Parser TickyFrame
-parseFrame =
-    TickyFrame
-     <$> spacesThen integer
-     <*> spacesThen integer
-     <*> spacesThen integer
-     <*> spacesThen (do n <- integer
-                        count (fromIntegral n) anyChar)
-     <*> spacesThen ((text "TOP" *> pure Top) <|> try nonExportedName <|> exportedName)
-     <*  eof
-  where
-    funcName = many $ alphaNum <|> oneOf "$=<>[]()+-,.#*|/_'"
-    sig = named "signature" $ braces $ many $ noneOf "}"
-
-    -- e.g. ghc-7.11:CmdLineParser.runCmdLine{v rWL}
-    exportedName :: Parser StgName
-    exportedName = named "exported name" $ do
-        mod <- parseModuleName
-        _name <- funcName
-        _sig <- sig
-        return $ StgName True mod _name _sig Nothing
-
-    -- e.g. sat_sbMg{v} (main@main:Main) in sbMh
-    nonExportedName = named "non-exported name" $ do
-        --_name <- funcName
-        _name <- many $ noneOf "{"
-        _sig <- sig
-        spaces
-        mod <- parens parseModuleName
-        parent <- optional $ text "in" *> spaces *> funcName
-        return $ StgName False mod _name _sig parent
-
-type QualifiedName = (ModuleName, String)
-
-startsWith :: Parser a -> Parser a -> Parser [a]
-startsWith first others = (:) <$> first <*> many others
-
-parseModuleName :: Parser ModuleName
-parseModuleName = named "module name" $ do
-    package <- packageName
-    optional $ char '@' >> packageName  -- what is this?
-    char ':'
-    name <- startsWith upper $ alphaNum <|> oneOf "_."
-    return $ ModName package name
-  where packageName = many $ alphaNum <|> oneOf "-_."
+import TickyReport
 
 delta :: Real a => a -> a -> Double
 delta a b
@@ -167,7 +36,7 @@ deltasTable as bs (metricName, metric) cols = header ++ dataRows
     header = [ Cells $ ["% change", metricName++" A", metricName++" B"]++map fst cols
              , HeaderSep
              ]
-    dataRows = 
+    dataRows =
         map formatRow
         $ sortBy (flip compare)
         $ tabulateDeltas as bs metric
