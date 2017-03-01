b7f228ae58eeeb8a823666e41a9a5d718d257c8c
Ben Gamari <ben@smart-cactus.org>
2017-03-01 13:50:35 -0500

    Rework nofib-commits
    Based on nofib-all found on ben-server

--- nofib-commits.sh
+++ nofib-commits.sh
@@ -1,22 +1,39 @@
 #!/bin/bash -e
 
-mkdir -p logs
+commits=$(git rev-parse --reverse $@)
+if [[ -z "$nruns" ]]; then
+    nruns=1
+fi
 
-do_commit() {
-    commit=$1
-    sha=`git rev-parse $commit`
-    echo "Building $1 ($commit)..."
+mkdir -p logs
+echo "Testing $(echo $commits | wc -l) commits with $nruns runs each"
+i=0
+root=$(pwd)
+for commit in $commits; do
+    echo "checking out $commit..."
     git checkout $commit
     git submodule update --init
-    ./validate --build-only >| logs/build-$sha.log
 
-    echo "Nofibbing $1 ($commit)..."
-    pushd nofib
+    echo "building $commit..."
+    make -j9 stage=2 >logs/build-$commit.log 2>&1
+    if [ ! -e inplace/bin/ghc-stage2 ]; then
+        echo "Uh oh, no compiler found!"
+        exit 1
+    fi
+
+    echo "running nofib on $commit..."
+    cd nofib
+    cabal install --with-ghc=$root/inplace/bin/ghc-stage2 --disable-library-profiling --force-reinstalls regex-compat html
     make clean
-    make boot
-    make >| logs/nofib-$sha.log 2>&1
-}
-
-for commit in $@; do
-    do_commit || echo "commit $commit failed"
+    make -j8 boot >logs/nofib-boot-$commit.log 2>&1
+    for run in $(seq $nruns); do
+        echo "  run $i"
+        (
+          git -C .. show --pretty=oneline
+          make
+        ) >logs/nofib-$(printf "%03d" $i)-$commit-run$(printf "%03d" $run).log 2>&1
+    done
+    let i=i+1
+    cd ..
 done
+
