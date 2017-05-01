6d9c8a792b93e84add579dfab8844e2dc473662e
Ben Gamari <ben@smart-cactus.org>
2017-05-01 19:14:35 -0400

    split-core2core: Handle multiple files on command line

--- split-core2core.py
+++ split-core2core.py
@@ -1,23 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
 import os
 import re
 
-f = sys.argv[1]
-base = os.path.splitext(f)[0]
-i = 0
+def split_file(f):
+    base = os.path.splitext(f)[0]
+    i = 0
 
-os.mkdir('%s.split' % f)
-output = open('%s.split/%s-00' % (f, base), 'w')
-for ln in open(f):
-    if ln.startswith('======='):
-        name = ln.strip('= ')
-        name = re.match(r'[a-z0-9 ]*', name.lower()).group(0)
-        name = name.strip().replace(' ', '-')
-        i += 1
-        print i, name
-        output = open('%s.split/%s.%02d-%s' % (f, base, i, name), 'w')
+    print(f)
+    os.mkdir('%s.split' % f)
+    output = open('%s.split/%s-00' % (f, base), 'w')
+    for ln in open(f):
+        if ln.startswith('======='):
+            name = ln.strip('= ')
+            name = re.match(r'[a-z0-9 ]*', name.lower()).group(0)
+            name = name.strip().replace(' ', '-')
+            i += 1
+            print(i, name)
+            output = open('%s.split/%s.%02d-%s' % (f, base, i, name), 'w')
 
-    output.write(ln)
+        output.write(ln)
 
+if __name__ == '__main__':
+    for f in sys.argv[1:]:
+        split_file(f)
