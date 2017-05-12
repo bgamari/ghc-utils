494c97df0a0b506a3676aaac7dc22263c320680d
Ben Gamari <ben@smart-cactus.org>
2017-05-11 21:33:45 -0400

    Fix location of autoconf sources

--- rel-eng/update-autoconf.sh
+++ rel-eng/update-autoconf.sh
@@ -2,13 +2,13 @@
 
 tmp=$(tempfile)
 
-curl -s https://git.savannah.gnu.org/cgit/autoconf.git/plain/build-aux/config.guess > $tmp
+curl -s 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' > $tmp
 for i in $(git ls-files | grep config.guess); do
     echo $i
     cp $tmp $i
 done
 
-curl -s https://git.savannah.gnu.org/cgit/autoconf.git/plain/build-aux/config.sub > $tmp
+curl -s 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD' > $tmp
 for i in $(git ls-files | grep config.sub); do
     echo $i
     cp $tmp $i
