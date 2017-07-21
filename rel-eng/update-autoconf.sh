15fa2cf49c7ae3d5f8af9a94f6a1f835bb671cd0
Ben Gamari <ben@smart-cactus.org>
2017-07-21 17:58:46 -0400

    update-autoconf: Update script

--- rel-eng/update-autoconf.sh
+++ rel-eng/update-autoconf.sh
@@ -1,17 +1,22 @@
 #!/bin/bash -e
 
-tmp=$(tempfile)
+tmp=`mktemp -d`
+git -C $tmp clone git://git0.savannah.gnu.org/config.git
+commit=`git -C $tmp/config rev-parse HEAD`
+echo "Updating to $commit..."
 
-curl -s 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' > $tmp
+files=
 for i in $(git ls-files | grep config.guess); do
     echo $i
-    cp $tmp $i
+    cp $tmp/config/config.guess $i
+    files="$i $files"
 done
 
-curl -s 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD' > $tmp
 for i in $(git ls-files | grep config.sub); do
     echo $i
-    cp $tmp $i
+    cp $tmp/config/config.sub $i
+    files="$i $files"
 done
 
-rm $tmp
+git commit $files -m "Update autoconf scripts" -m "Scripts taken from autoconf $commit"
+rm -Rf $tmp
