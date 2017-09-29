04f0c48355e8ab8749b2f949703d3c6dba7f6f0e
Ben Gamari <ben@smart-cactus.org>
2017-09-28 22:04:52 -0400

    bin-release: Use gcc-7 on OS X

--- rel-eng/bin-release.sh
+++ rel-eng/bin-release.sh
@@ -134,7 +134,7 @@ setup_env() {
             # Only Sierra supports clock_gettime. See #12858.
             log "Disabling clock_gettime"
             export ac_cv_func_clock_gettime=no
-            configure_opts="$configure_opts CC=/usr/local/bin/gcc-6"
+            configure_opts="$configure_opts CC=/usr/local/bin/gcc-7"
             log "Using Homebrew's gcc $(gcc -dumpversion)"
             ;;
         FreeBSD)
