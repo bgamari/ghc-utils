e0f17ca81b92729e68081019592a399bb4d4c3d9
Ben Gamari <ben@smart-cactus.org>
2017-09-29 11:40:17 -0400

    Another workaround for the FreeBSD linker bug
    The workaround I used previously was not to use ld.bfd (which also breaks for
    unrelated reasons); rather we instead need to disable split sections.

--- rel-eng/bin-release.sh
+++ rel-eng/bin-release.sh
@@ -138,11 +138,6 @@ setup_env() {
             log "Using Homebrew's gcc $(gcc -dumpversion)"
             ;;
         FreeBSD)
-            if uname -a | grep "10.3-RELEASE"; then
-                # The gold version in FreeBSD 10.3 is affected by
-                # https://sourceware.org/bugzilla/show_bug.cgi?id=12771
-                configure_opts="$configure_opts --disable-ld-override"
-            fi
             log "Disabling large address space support."
             configure_opts="$configure_opts --disable-large-address-space"
             make=gmake
@@ -191,6 +186,13 @@ EOF
             log "using in-tree GMP"
             echo 'libraries/integer-gmp_CONFIGURE_OPTS += --configure-option=--with-intree-gmp' >> mk/build.mk
             ;;
+        FreeBSD)
+            if uname -a | grep "10.3-RELEASE"; then
+                # The gold version in FreeBSD 10.3 is affected by
+                # https://sourceware.org/bugzilla/show_bug.cgi?id=12771
+                echo "SplitSections=NO" >>build.mk
+                echo "Disabling SplitSections due to FreeBSD gold bug"
+            fi
     esac
 
     if which dblatex; then
