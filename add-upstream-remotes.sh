97a5fd4aa684389bb2a0ec30f95a8e87e5e0dbd0
Ben Gamari <ben@smart-cactus.org>
2018-06-15 17:11:15 -0400

    Fix add-upstream-remotes

--- add-upstream-remotes.sh
+++ add-upstream-remotes.sh
@@ -24,16 +24,16 @@ for s in `git submodule | awk '{print $2'}`; do
 done
 add utils/hpc git@github.com:haskell/hpc
 
-#add libraries/Cabal git://github.com/haskell/Cabal
-#add libraries/bytestring git://github.com/haskell/bytestring
-#add libraries/containers git://github.com/haskell/containers
-#add libraries/haskeline git://github.com/judah/haskeline
-#add libraries/win32 git://github.com/haskell/Win32
-#add libraries/binary git@github.com:kolmodin/binary
-#add libraries/directory git://github.com/haskell/directory
-#add libraries/process git://github.com/haskell/process
-#add libraries/time git://github.com/haskell/time
-#add libraries/unix git@github.com:haskell/unix
-#add libraries/primitive git@github.com:haskell/primitive
-#add utils/haddock git@github.com:haskell/haddock
+add libraries/Cabal git://github.com/haskell/Cabal
+add libraries/bytestring git://github.com/haskell/bytestring
+add libraries/containers git://github.com/haskell/containers
+add libraries/haskeline git://github.com/judah/haskeline
+add libraries/Win32 git://github.com/haskell/Win32
+add libraries/binary git@github.com:kolmodin/binary
+add libraries/directory git://github.com/haskell/directory
+add libraries/process git://github.com/haskell/process
+add libraries/time git://github.com/haskell/time
+add libraries/unix git@github.com:haskell/unix
+add libraries/primitive git@github.com:haskell/primitive
+add utils/haddock git@github.com:haskell/haddock
 
