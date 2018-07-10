d0fd4e56478f4ddd8e66e6b6fe899784ce55e39c
Ben Gamari <ben@smart-cactus.org>
2018-07-10 19:20:52 -0400

    gdb: Add gdbinit file

--- gdb/default.nix
+++ gdb/default.nix
@@ -1,11 +1,24 @@
+# Usage:  nix build gdbinit
+#
+# `source` the resulting file in gdb.
+
 { nixpkgs ? (import <nixpkgs> {}) }:
 
 with nixpkgs;
 rec {
   ghc-gdb = pythonPackages.buildPythonPackage {
     name = "ghc-gdb";
+    doCheck = false;
     src = ./.;
   };
   env = python.withPackages (_: [ ghc-gdb ]);
+  gdbinit = writeTextFile {
+    name = "gdbinit";
+    text = ''
+      python sys.path += ["${env}/lib/python2.7/site-packages"]
+      python import ghc_gdb
+      echo The `ghc` command is now available.\n
+    '';
+  };
 }
 
