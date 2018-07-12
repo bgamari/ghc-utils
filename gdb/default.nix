5b479f81c37000ba89dddcd60b72b304414a51fa
Ben Gamari <ben@smart-cactus.org>
2018-07-11 20:20:46 -0400

    gdb: Make nix expression more convenient

--- gdb/default.nix
+++ gdb/default.nix
@@ -2,16 +2,32 @@
 #
 # `source` the resulting file in gdb.
 
-{ nixpkgs ? (import <nixpkgs> {}) }:
+#{ nixpkgs ? (import <nixpkgs> {}) }:
 
-with nixpkgs;
-rec {
+let
+  rev = "140ad12d71c57716b3ee3b777d53c27b019360f0";
+  baseNixpkgs =
+    builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    sha256 = "088wzwgm7nqjv2bpirmjsncysjvzjd237z7d0lm2jzng81f4a6ll";
+  };
+  nixpkgs = baseNixpkgs;
+
+in with nixpkgs; rec {
+  pythonPackages = python3Packages;
+
   ghc-gdb = pythonPackages.buildPythonPackage {
     name = "ghc-gdb";
     doCheck = false;
     src = ./.;
   };
+
+  gdb = nixpkgs.gdb.override {
+    python = python3;
+  };
+
   env = python.withPackages (_: [ ghc-gdb ]);
+
   gdbinit = writeTextFile {
     name = "gdbinit";
     text = ''
