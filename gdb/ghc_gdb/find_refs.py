a0e144776cee73c508df07c7e19ad54a21d28527
Ben Gamari <ben@smart-cactus.org>
2018-07-15 21:20:29 -0400

    gdb: More work on find_refs

--- gdb/ghc_gdb/find_refs.py
+++ gdb/ghc_gdb/find_refs.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+from . import ghc_heap
 import gdb
 
 word_size = 8
@@ -22,7 +23,7 @@ heap_start = 0x4200000000
 heap_end = 0x4210000000
 
 def search_memory_many(inferior: gdb.Inferior, start: Ptr, end: Ptr, val: Ptr) -> List[Ptr]:
-    print('Searching for %s' % val)
+    #print('Searching for %s' % val)
     while True:
         addr = inferior.search_memory(start, end-start, val)
         if addr is None:
@@ -46,6 +47,9 @@ def find_symbol(addr: Ptr) -> Optional[gdb.Symbol]:
     except RuntimeError:
         return None
 
+    if block is None:
+        return None
+
     while block and not block.function:
         block = block.superblock
     return block.function
@@ -62,11 +66,14 @@ def find_refs_rec(closure_ptr: Ptr, depth: int):
     """
     Recursively search for references to a closure up to the given depth.
     """
+    inf = gdb.selected_inferior()
     if depth == 0:
         return []
     else:
-        return [(ref, find_refs_rec(ref, depth-1))
-                for ref in find_refs(closure_ptr)]
+        return [(ref, find_refs_rec(ref_start, depth-1))
+                for ref in find_refs(closure_ptr)
+                for ref_start in [find_containing_closure(inf, ref)]
+                if ref_start is not None]
 
 def find_containing_closure(inferior: gdb.Inferior, ptr: Ptr) -> Optional[Ptr]:
     """
@@ -81,7 +88,13 @@ def find_containing_closure(inferior: gdb.Inferior, ptr: Ptr) -> Optional[Ptr]:
         bs = inferior.read_memory(start, word_size)
         addr = unpack_pointer(bs)
         sym = find_symbol(addr)
-        if sym is not None and sym.print_name.endswith('_info'):
-            return Ptr(start)
+        # Is this an info table pointer?
+        if sym is not None and sym.print_name.endswith('_info'): # and sym.value == addr:
+            info = ghc_heap.get_itbl(gdb.parse_and_eval('(StgClosure *) %d' % start))
+            nptrs = int(info['layout']['payload']['ptrs'])
+            if i <= nptrs:
+                return Ptr(start)
+            else:
+                print('suspicious info table: too far (p=0x%08x, info=%s, nptrs=%d, i=%d)' % (start, sym.print_name, nptrs, i))
 
     return None
