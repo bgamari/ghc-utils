e8272c2e9227892c64289e59016e6eb4d46fc727
Ben Gamari <ben@smart-cactus.org>
2018-07-20 15:18:50 -0400

    gdb: Fix Ptr

--- gdb/ghc_gdb/types.py
+++ gdb/ghc_gdb/types.py
@@ -5,22 +5,25 @@ else:
     TAG_MASK = 3
 
 class Ptr(int):
+    def __init__(self, addr: int):
+        self.addr = addr
+
     def __str__(self):
-        return hex(self)
+        return hex(self.addr)
 
     def __repr__(self):
-        return hex(self)
+        return hex(self.addr)
 
     def untagged(self):
-        return self & ~TAG_MASK
+        return Ptr(self.addr & ~TAG_MASK)
 
     def tagged(self, tag: int) -> "Ptr":
-        assert tag < 8
-        return self | ~Ptr(tag)
+        assert tag < TAG_MASK+1
+        return Ptr(self.addr | ~tag)
 
     def pack(self) -> bytes:
         import struct
-        return struct.pack('@P', ptr)
+        return struct.pack('@P', self.addr)
 
     @staticmethod
     def unpack(bs: bytes) -> "Ptr":
