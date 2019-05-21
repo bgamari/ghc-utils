b0516e81b933fccdf88e814673c2f33703b21224
Ben Gamari <ben@smart-cactus.org>
2019-05-21 16:04:00 -0400

    Fix mut_list

--- gdb/ghc_gdb/mut_list.py
+++ gdb/ghc_gdb/mut_list.py
@@ -4,12 +4,15 @@ import gdb
 
 def collect_mut_list() -> Iterator[Ptr]:
     ncaps = int(gdb.parse_and_eval('n_capabilities'))
-    ngens = int(gdb.parse_and_eval('RtsFlags.GcFlags.n_generations'))
+    ngens = int(gdb.parse_and_eval('RtsFlags.GcFlags.generations'))
     for c in range(ncaps):
         for g in range(ngens):
-            bd = gdb.parse_and_eval('capabilities[%d].mut_lists[%d]', ngens)
+            bd = gdb.parse_and_eval('capabilities[%d].mut_lists[%d]' % (c, g))
             while Ptr(bd) != Ptr(0):
-                p = bd['start']
-                end = bd['scan']
+                p = bd.dereference()['start']
+                end = bd.dereference()['free']
                 while p < end:
-                    yield Ptr(p.deref()).untagged()
+                    yield Ptr(p.dereference()).untagged()
+                    p += 1
+
+                bd = bd['link']
